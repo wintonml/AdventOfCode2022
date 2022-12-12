9d636c460c1f7da2c58aa76ceccdade2ab1ee331
Michael Lennon Winton <wintonml@gmail.com>
2022-12-12 20:58:57 +1300

    Part two answers

--- Day1/day1.py
+++ Day1/day1.py
@@ -10,5 +10,21 @@ def part_one():
         elf_calories.append(sum(calories_carried))
 
     max_calories_carried = max(elf_calories)
-    print(max_calories_carried)
+    print(f"Max amount of calories: {max_calories_carried}")
+
     f.close()
+
+    return elf_calories
+
+
+def part_two():
+    top_elves_of_interest = 3
+    calories_carried = part_one()
+    calories_carried.sort(reverse=True)  # descending order
+
+    top_elves = calories_carried[0:top_elves_of_interest]
+    print(f"Max calories of top {top_elves_of_interest}: {sum(top_elves)}")
+
+
+if __name__ == '__main__':
+    part_two()
