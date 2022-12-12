1e486959bb4b68f6783e860d89d7738e71830300
Michael Lennon Winton <wintonml@gmail.com>
2022-12-12 23:00:19 +1300

    Day2 part 2 answer

--- Python/Day2/day2.py
+++ Python/Day2/day2.py
@@ -25,20 +25,39 @@ elf_two_dict = {
     "Z": "Scissors",
 }
 
+desired_result_dict = {
+    "X": "Loss",
+    "Y": "Draw",
+    "Z": "Win",
+}
+
 
 def part_one():
-    f = open('EncryptedStrategyGuide.txt', 'r')
-    content = f.read()
-    games = content.split("\n")
+    print("Part 1 Challenge")
+    complete_challenge(1)
+
+
+def part_two():
+    print("\nPart 2 Challenge")
+    complete_challenge(2)
+
+
+def complete_challenge(challenge_number):
+    games = get_games()
 
     elf_one_points = 0
     elf_two_points = 0
 
     for game in games:
-        elf_one, elf_two = game.split(' ', 1)
+        elf_one, column_two = game.split(' ', 1)
         elf_one_shape = elf_one_dict[elf_one]
-        elf_two_shape = elf_two_dict[elf_two]
-        results = match_result(elf_one_shape, elf_two_shape)
+
+        if challenge_number == 1:
+            elf_two_shape = elf_two_dict[column_two]
+            results = match_result(elf_one_shape, elf_two_shape)
+        else:
+            result = desired_result_dict[column_two]
+            results = match_result(elf_one_shape, game_result=result)
 
         elf_one_points += results[0]
         elf_two_points += results[1]
@@ -46,16 +65,32 @@ def part_one():
     print(f"Elf one: {elf_one_points}")
     print(f"Elf two: {elf_two_points}")
 
-    print(f"Total points: {elf_one_points + elf_two_points}")
+
+def get_games():
+    f = open('EncryptedStrategyGuide.txt', 'r')
+    content = f.read()
+    return content.split("\n")
 
 
-def part_two():
-    return True
+def find_player_two_shape(player_one_value, game_result):
+    if game_result == "Draw":
+        player_two_value = player_one_value
+    elif game_result == "Win":
+        potential_value = player_one_value + 1
+        player_two_value = potential_value if potential_value != 4 else 1
+    elif game_result == "Loss":
+        potential_value = player_one_value - 1
+        player_two_value = potential_value if potential_value != 0 else 3
+    return player_two_value
 
 
-def match_result(player_one_shape, player_two_shape):
+def match_result(player_one_shape, player_two_shape=-100, game_result=None):
     player_one_value = ShapePoints[player_one_shape].value
-    player_two_value = ShapePoints[player_two_shape].value
+    if game_result is None:
+        player_two_value = ShapePoints[player_two_shape].value
+    else:
+        player_two_value = find_player_two_shape(player_one_value,
+                                                 game_result)
     player_difference = player_two_value - player_one_value
 
     # Draw condition
@@ -69,7 +104,7 @@ def match_result(player_one_shape, player_two_shape):
         player_two_points = player_two_value + ResultsPoints.Win.value
 
     # Lose condition is difference of -1 or 2
-    else:
+    elif player_difference in (-1, 2):
         player_one_points = player_one_value + ResultsPoints.Win.value
         player_two_points = player_two_value + ResultsPoints.Loss.value
 
@@ -78,3 +113,4 @@ def match_result(player_one_shape, player_two_shape):
 
 if __name__ == '__main__':
     part_one()
+    part_two()
